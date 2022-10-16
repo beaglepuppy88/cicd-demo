c4b72c3fa29ba8f61a498092bb57070f4db73778
beaglepuppy88 <atieu@yahoo.com>
2022-10-16 16:58:44 -0400

    add tests

---
+++ src/test/groovy/codes/recursive/AbstractSpec.groovy
@@ -0,0 +1,7 @@
+package codes.recursive
+import io.micronaut.context.ApplicationContext
+import spock.lang.AutoCleanup
+import spock.lang.Shared
+import spock.lang.Specification
+class AbstractSpec extends Specification {
+}
